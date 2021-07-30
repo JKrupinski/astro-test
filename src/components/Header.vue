4b94e653246385a931fd991c9e0e3a8b94785b9c
Jarek <jarek.krupinski@snow.dog>
2021-07-30 19:25:59 +0200

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <Navigation />
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Navigation from './Navigation.vue'
+
+export default defineComponent({
+    setup () {
+        return {}
+    }
+})
+</script>
+
+<style>
+
+</style>
